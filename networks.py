d94a9fba562789271d92805457f53a9238c0dddc
D3v <deviljin112@users.noreply.github.com>
2020-12-03 16:36:21 +0000

    Create networks.py

---
+++ networks.py
@@ -0,0 +1,26 @@
+from ipaddress import ip_interface
+
+# TEST MATERIAL
+# 192.168.200.5/30 and 192.168.200.9/30 == FALSE
+# 192.168.200.5/30 and 192.168.200.6/30 == TRUE
+
+print("-=Network Checker=-")
+
+print("Please insert the first IP Address")
+print("Format: X.X.X.X/Y")
+first = input("=> ")
+
+print("Please input the second IP Address")
+print("Format: X.X.X.X/Y")
+second = input("=> ")
+
+ip_1 = ip_interface(first)
+ip_2 = ip_interface(second)
+
+
+ans = ip_2.network.overlaps(ip_1.network)
+
+if ans:
+    print("These IPs are on the same network.")
+else:
+    print("These IPs are NOT on the same network.")
